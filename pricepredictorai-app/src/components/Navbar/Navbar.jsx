9ba8b5a370e8b8e9f5d2eddf07442d2851af7d54
Puneet Chaudhary <chaudhap@uoguelph.ca>
2025-06-24 19:40:06 -0400

    added navbar to the frontend

---
+++ pricepredictorai-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar">
+        <h1>Price Predictor AI</h1>
+        <ul className="nav-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">How to use?</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <ul className="auth-links">
+          <li>
+            <a href="#">Login</a>
+          </li>
+          <li>
+            <a href="#">Sign Up</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar
